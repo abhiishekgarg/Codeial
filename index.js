53d382ab5fdc28c91515b7f9c6c1526bfefb03d4
Abhishek <abhiishekgarg@gmail.com>
2020-10-18 14:44:06 +0530

    Create routes folder

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+//use express router
+app.use('/', require('./routes'));
 
 app.listen(port, function(err)
 {
---
+++ routes/index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+
+const router = express.Router();
+
+console.log('Router loaded');
+
+module.exports = router;
