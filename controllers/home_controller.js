489e3d41ecca30d83019e63a280e584b4b459ad3
Abhishek <abhiishekgarg@gmail.com>
2020-10-20 02:21:19 +0530

    Create a view for home

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,6 +1,9 @@
 module.exports.home = function(req, res)
 {
-    return res.end('<h1>Express is up for Codeial!</h1>');
+    return res.render('home',
+    {
+        title: 'Home'
+    });
 }
 
 // module.exports.actionName = function(req, res){}
--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = 8000;
 app.use('/', require('./routes'));
 
 // set up the view engine
-app.set('view enginer', 'ejs');
+app.set('view engine', 'ejs');
 app.set('views', './views');
 
 app.listen(port, function(err)
