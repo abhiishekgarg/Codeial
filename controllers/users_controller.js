029650b2598d352df08b3fd0aa76d80d3be622f5
Abhishek <abhiishekgarg@gmail.com>
2020-10-20 02:40:20 +0530

    Add views for posts and profiles

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,4 +1,7 @@
 module.exports.profile = function(req, res)
 {
-    res.end('<h1>User Profile</h1>');
+    return res.render('profile',
+    {
+        title: 'Profile'
+    });
 }
--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const homeController = require('../controllers/home_controller');
 
 router.get('/', homeController.home);
+router.get('/home', homeController.home);
 router.use('/users', require('./users'));
 
 // for any further routes, access from here
