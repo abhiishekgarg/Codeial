e98fb8f22d63aab4090c8c21b5dc34e7daab4412
Abhishek <abhiishekgarg@gmail.com>
2020-10-20 02:12:06 +0530

    Integrate views with EJS

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const port = 8000;
 //use express router
 app.use('/', require('./routes'));
 
+// set up the view engine
+app.set('view enginer', 'ejs');
+app.set('views', './views');
+
 app.listen(port, function(err)
 {
     if(err)
