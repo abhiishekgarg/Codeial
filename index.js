e62d61093583cd8957aa30339ef607eba984e4ab
Abhishek <abhiishekgarg@gmail.com>
2020-10-21 00:05:19 +0530

    Link MongoDB using mongoose

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8000;
 const expressLayouts = require('express-ejs-layouts');
+const db = require('./config/mongoose');
 
 app.use(express.static('./assets'));
 
