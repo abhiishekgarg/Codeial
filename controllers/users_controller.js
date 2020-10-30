6c706743ee74e870cfbdc5c2f68dcd764a263420
Abhishek Garg <abhiishekgarg@gmail.com>
2020-10-30 20:50:35 +0530

    Create Sign Out

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -63,3 +63,9 @@ module.exports.createSession = function(req, res)
 {
    return res.redirect('/users/profile');
 }
+
+module.exports.destroySession = function(req, res)
+{
+    req.logout();
+    return res.redirect('/');
+}
--- routes/users.js
+++ routes/users.js
@@ -23,5 +23,8 @@ router.post('/create-session', passport.authenticate(
     {failureRedirect: '/users/sign-in'},
 ), usersController.createSession);
 
+router.get('/sign-out', usersController.destroySession);
+
+
 
 module.exports = router;
