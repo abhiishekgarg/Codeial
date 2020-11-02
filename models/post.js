ffec324b3e2206394eab59f17147ead13bdb7fdf
Abhishek Garg <abhiishekgarg@gmail.com>
2020-11-03 01:59:11 +0530

    Creating Schema for Comments

--- models/post.js
+++ models/post.js
@@ -12,7 +12,15 @@ const postSchema = new mongoose.Schema
         {
             type: mongoose.Schema.Types.ObjectId,
             ref: 'User'
-        }
+        },
+        // include the array of ids of all comments in this post schema itself
+        comments:
+        [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'Comment'
+            }
+        ]
     },
     {
         timestamps: true
