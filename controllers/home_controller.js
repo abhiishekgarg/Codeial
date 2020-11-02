d3e52e705316c91ba96ee109abe72ccbaa2fb1cb
Abhishek Garg <abhiishekgarg@gmail.com>
2020-11-03 00:37:04 +0530

    Display Posts and related User

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,9 +1,25 @@
+const Post = require('../models/post');
+
 module.exports.home = function(req, res)
 {
     // console.log(req.cookies);
-    return res.render('home',
+    // Post.find({}, function(err, posts)
+    // {
+    //     return res.render('home',
+    //     {
+    //         title: 'Codeial | Home',
+    //         posts: posts
+    //     });
+    // });
+
+    // populate the user of each post
+    Post.find({}).populate('user').exec(function(err, posts)
     {
-        title: 'Codeial | Home'
+        return res.render('home',
+        {
+            title: 'Codeial | Home',
+            posts: posts
+        });
     });
 }
 
