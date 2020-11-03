94987a533811d578b8aa917b7e4abd004dac90cd
Abhishek Garg <abhiishekgarg@gmail.com>
2020-11-03 15:22:31 +0530

    Nest Population :: Display Comments

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -13,7 +13,17 @@ module.exports.home = function(req, res)
     // });
 
     // populate the user of each post
-    Post.find({}).populate('user').exec(function(err, posts)
+    Post.find({})
+    .populate('user')
+    .populate
+    ({
+        path: 'comments',
+        populate:
+        {
+            path: 'user'
+        }
+    })
+    .exec(function(err, posts)
     {
         return res.render('home',
         {
