bdf5a896a185f1481a86cbf4797e514745186084
Abhishek Garg <abhiishekgarg@gmail.com>
2020-11-04 00:41:54 +0530

    Delete a Comment (Authorized)

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -30,3 +30,35 @@ module.exports.create = function(req, res)
     });
 }
 
+module.exports.destroy = function(req, res)
+{
+    Comment.findById(req.params.id, function(err, comment)
+    {
+        // .id means converting the object id into string
+        var postCreatorId;
+        Post.findById(comment.post, function(err, post)
+        {
+            postCreatorId = post.user;
+            console.log(post.user);
+            console.log(postCreatorId);
+            console.log(req.user.id);
+            console.log(req.user._id);
+        });
+        if(comment.user == req.user.id ||  postCreatorId === req.user.id)
+        {
+            let postId = comment.post;
+            
+            comment.remove();
+
+            Post.findByIdAndUpdate(postId, { $pull: {comments: req.params.id}}, function(err, post)
+            {
+                return res.redirect('back');
+            });
+        }
+        else
+        {
+            return res.redirect('back');
+        }
+    });
+}
+
--- routes/comments.js
+++ routes/comments.js
@@ -6,6 +6,7 @@ const passport = require('passport')
 const commentsController = require('../controllers/comments_controller');
 
 router.post('/create', passport.checkAuthentication, commentsController.create);
+router.get('/destroy/:id', passport.checkAuthentication, commentsController.destroy);
 
 
 module.exports = router;
