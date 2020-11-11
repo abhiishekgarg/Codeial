1c00795125d4746ae2debfc09a3ce03122b4d352
Abhishek Garg <abhiishekgarg@gmail.com>
2020-11-12 03:19:13 +0530

    Setting up directory structure (APIs)

--- routes/index.js
+++ routes/index.js
@@ -11,6 +11,8 @@ router.use('/users', require('./users'));
 router.use('/posts', require('./posts'));
 router.use('/comments', require('./comments'));
 
+router.use('/api', require('./api'));
+
 // for any further routes, access from here
 // router.use('routerName, require('./routerFile');
 
