c5c081d72365dbf3340614fb7cd8c918162001d5
Abhishek Garg <abhiishekgarg@gmail.com>
2020-11-12 23:58:45 +0530

    Creating a Token

---
+++ controllers/api/v1/users_api.js
@@ -0,0 +1,35 @@
+const User = require('../../../models/users');
+const jwt = require('jsonwebtoken');
+
+module.exports.createSession = async function(req, res)
+{
+    try
+    {
+        let user = await User.findOne({email: req.body.email});
+        
+        if(!user || user.password != req.body.password)
+        {
+            return res.json(422, 
+            {
+                message: 'Invalid Username/Password'
+            });
+        }
+
+        return res.json(200, 
+        {
+            message: 'Sign in successful, here is your token, please keep it safe!',
+            data: 
+            {
+                token: jwt.sign(user.toJSON(), 'codeial', {expiresIn: '10000'})
+            }
+        });
+    }
+    catch(err)
+    {
+        console.log('********', err);
+        return res.json(500, 
+        {
+            message: 'Internal Server Error'
+        });
+    }
+}
--- routes/api/v1/index.js
+++ routes/api/v1/index.js
@@ -3,5 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.use('/posts', require('./posts'));
+router.use('/users', require('./users'));
+
 
 module.exports = router;
