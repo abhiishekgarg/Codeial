be9b6605e104d6984b1d734d97f88c00d592d1b0
Abhishek Garg <abhiishekgarg@gmail.com>
2020-11-12 22:48:18 +0530

    Setting Up Passport JWT

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const db = require('./config/mongoose');
 const session = require('express-session');
 const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
+const passportJWT = require('./config/passport-jwt-strategy');
 const MongoStore = require('connect-mongo')(session);
 const sassMiddleware = require('node-sass-middleware');
 const flash = require('connect-flash');
