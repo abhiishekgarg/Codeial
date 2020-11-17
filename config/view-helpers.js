ac54ba6d125cd2c6435af65300f13515acccd023
Abhishek Garg <abhiishekgarg@gmail.com>
2020-11-17 15:32:34 +0530

    View Helpers

---
+++ config/view-helpers.js
@@ -0,0 +1,15 @@
+const env = require('./environment');
+const fs = require('fs');
+const path = require('path');
+
+module.exports = (app) =>
+{
+    app.locals.assetPath = function(filePath)
+    {
+        if(env.name == 'development')
+        {
+            return filePath;
+        }
+        return '/' + JSON.parse(fs.readFileSync(path.join(__dirname, '../public/assets/rev-manifest.json')))[filePath];
+    }
+}
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const env = require('./config/environment');
 // const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const app = express();
+require('./config/view-helpers')(app);
 const port = 8000;
 const expressLayouts = require('express-ejs-layouts');
 const db = require('./config/mongoose');
