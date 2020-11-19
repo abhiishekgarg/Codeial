f056f0bcc2a188f572f9f3756f2eb65106c4bbee
Abhishek Garg <abhiishekgarg@gmail.com>
2020-11-20 01:40:55 +0530

    Setting Up Socket.io

---
+++ config/chat_sockets.js
@@ -0,0 +1,6 @@
+
+
+module.exports.chatSockets = function(socketServer)
+{
+
+}
--- index.js
+++ index.js
@@ -15,11 +15,16 @@ const passportJWT = require('./config/passport-jwt-strategy');
 const passportGoogle = require('./config/passport-google-oauth2-strategy');
 const path = require('path');
 
-
 const MongoStore = require('connect-mongo')(session);
 const sassMiddleware = require('node-sass-middleware');
 const flash = require('connect-flash');
 const customMware = require('./config/middleware');
+
+// setup the chat server to be used with socket.io
+const chatServer = require('http').Server(app);
+const chatSockets = require('./config/chat_sockets').chatSockets(chatServer);
+chatServer.listen(5000);
+console.log(`Chat server is running on port: 5000`);
 
 if(env.name == 'development')
 {
